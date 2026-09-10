a80cf956a0a3410d7d47122e012d395f9dbcfff3
Sakshi <savipanwar0@gmail.com>
2026-09-10 13:53:21 +0530

    DB Password secured

--- config.py
+++ config.py
@@ -1,5 +1,23 @@
-class Config:
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
+
+
+
+
+
+
+
+
+
+"""class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'shinchan@123'
-    MYSQL_DB = 'flask_crud'
+    MYSQL_DB = 'flask_crud' """
